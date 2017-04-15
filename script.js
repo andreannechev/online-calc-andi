1f5f8b892dbc93925f23eeae97910401df3aa996
Andrean Nechev <andrean.nechev@gmail.com>
2017-04-15 16:39:32 +0300

    all buttons works

--- script.js
+++ script.js
@@ -8,7 +8,79 @@ $( document ).ready(function() {
     Ф, при натискане на някой оператор да запазва стринга и да създава нов стринг
     Ф, която да събере двата стринга и да покаже резултата
 */
-//1.
+/*1.
+var cifArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (var i = 0; i < cifArr.length; i++)
+  $("#1").click(function() {
+    console.log($(this).attr("value"));
+  });
+*/
+
+// add function
+var num1 = undefined;
+var num2 = undefined;
+function add(num1, num2) {
+  return(num1 + num2);
+}
+function subtract(num1, num2) {
+  return(num1 - num2);
+}
+function multiply(num1, num2) {
+  return(num1 * num2);
+}
+function devide(num1, num2) {
+  return(num1 / num2);
+}
+// add number listener
+$("#1").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#2").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#3").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#4").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#5").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#6").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#7").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#8").click(function() {
+  //$("#screen").html('');
+  $("#screen").append($(this).attr("value"));
+});
+$("#9").click(function() {
+  $("#screen").append($(this).attr("value"));
+});
+$("#0").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#decimal").click(function() {
+  console.log($(this).attr("value"));
+});
+// add operator listener
+$("#devide").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#multiply").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#subtract").click(function() {
+  console.log($(this).attr("value"));
+});
+$("#add").click(function() {
+  console.log($(this).attr("value"));
+});
+
+// store pressed buttons in array
 
 
 });// A $( document ).ready() block.
